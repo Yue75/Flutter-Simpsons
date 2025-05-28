f7b20128c059807cc104d67e8cb2d63df1bd47bc
Yue75 <buveusechallbroo95@gmail.com>
2025-05-28 17:11:14 +0200

    details personnages

--- lib/models/personnage.dart
+++ lib/models/personnage.dart
@@ -1,28 +1,34 @@
 class Personnage {
   final String name;
+  final String slug;
   final String image;
   final String description;
   final String cheveux;
   final String genre;
   final String occupation;
+  final String episodeId;  // si utile
 
   Personnage({
     required this.name,
+    required this.slug,
     required this.image,
     required this.description,
     required this.cheveux,
     required this.genre,
     required this.occupation,
+    required this.episodeId,
   });
 
   factory Personnage.fromMap(Map<String, dynamic> map) {
     return Personnage(
-      name: map['name'],
-      image: map['image'],
-      description: map['description'],
-      cheveux: map['cheveux'],
-      genre: map['genre'],
-      occupation: map['occupation'],
+      name: map['name'] ?? '',
+      slug: map['slug'] ?? '',
+      image: map['image'] ?? '',
+      description: map['description'] ?? '',
+      cheveux: map['cheveux'] ?? '',
+      genre: map['genre'] ?? '',
+      occupation: map['occupation'] ?? '',
+      episodeId: map['episodeId'] ?? '',
     );
   }
 }
